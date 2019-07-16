9c0c9ca0ace6b9ed535152bc6845b1adcba2b39b
flavio-bernasconi <info@flaviobernasconi.it>
2019-07-16 09:30:02 +0200

    second commit

--- simon/altv/main.js
+++ simon/altv/main.js
@@ -1,16 +1,6 @@
 
 var numeri = [];
 
-// for (var i = 0; i < 6; i++) {
-//   var randomNum = Math.floor(Math.random()* 100);
-//   if (!numeri.includes(randomNum)) {
-//     numeri.push(randomNum);
-//   }
-//   else {
-//     i--;
-//   }
-// }
-
 var i = 0;
 while(i < 6 ){
   var randomNum = Math.floor(Math.random()* 100);
@@ -30,29 +20,42 @@ while(i < 6 ){
 var result = confirm( numeri );
 var numeriUtente = [];
 var numeriTrovati = [];
+var errore = "";
 
 function mia(){
   if ( result ) {
       for (var i = 0; i < numeri.length; i++) {
         var numeriInseriti = parseInt(prompt());
-        if (!numeriUtente.includes(numeriInseriti)) {
+        //controlli input prompt
+        if (numeriInseriti > 100 || numeriInseriti.length > 2 || isNaN(numeriInseriti)) {
+          i--;
+          console.log("errore di sistema",numeriInseriti);
+          errore = true;
+        }
+        else {
+          errore = false;
+        }
+
+        //se il numero inserito dall utente NON è presente allora pushalo
+        if (!numeriUtente.includes(numeriInseriti) && errore == false) {
           numeriUtente.push(numeriInseriti);
+          if (numeriUtente.length > numeri.length ) {
+            break;
+          }
         }
         else {
           i--;
         }
 
+        //NUMERI TROVATI
         if (numeri.includes(numeriUtente[i])) {
-          numeriTrovati.push(numeriUtente[i]);
+            numeriTrovati.push(numeriUtente[i]);
+
         }
       }
       console.log("numeri da ricordare",numeri.sort());
       console.log("numeri inseriti dall utente",numeriUtente.sort());
       console.log("numeri trovati ",numeriTrovati.length + " : " +  numeriTrovati.sort());
-      // console.log("numeri non trovati: ",numeriNonTrovati.sort());
-      // console.log("numeri trovati: ",numeriTrovati.length);
-      // console.log("numeri non trovati: ",numeriNonTrovati.length);
-
 
     }
 
